72cb0c3f13179032e8f0e4093c0304b0f61061d2
Stephen Muteti <stephenmuteti09@gmail.com>
2025-12-30 12:19:56 +0300

    Initial commit

--- app/config.py
+++ app/config.py
@@ -1,10 +1,9 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        "postgresql+psycopg://buxton:bTP51pT1eYjCgbQ_APOSnw@"
+        "cockroachdb+psycopg://buxton:bTP51pT1eYjCgbQ_APOSnw@"
         "order-mgt-19894.j77.aws-ap-south-1.cockroachlabs.cloud:26257/"
         "order-mgt"
         "?sslmode=verify-full"
         "&sslrootcert=certs/root.crt"
     )
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
